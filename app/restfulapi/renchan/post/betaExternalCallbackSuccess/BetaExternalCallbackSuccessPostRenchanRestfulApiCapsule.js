cd7e382cca9f13fa57e12124178a86464f175572
Stew Eucen <stew.eucen@gmail.com>
2025-07-08 21:32:23 +0900

    Declare BetaExternalCallbackSuccessPostRenchanRestfulApiCapsule

---
+++ app/restfulapi/renchan/post/betaExternalCallbackSuccess/BetaExternalCallbackSuccessPostRenchanRestfulApiCapsule.js
@@ -0,0 +1,10 @@
+import BaseAppRenchanRestfulApiCapsule from '../../BaseAppRenchanRestfulApiCapsule.js'
+
+/**
+ * Beta external callback success POST Renchan RESTful API capsule.
+ *
+ * @extends {BaseAppRenchanRestfulApiCapsule}
+ */
+export default class BetaExternalCallbackSuccessPostRenchanRestfulApiCapsule extends BaseAppRenchanRestfulApiCapsule {
+  // noop
+}

980af7c1641fc633401f2a961591398b82537835
hieuthahn <hieuthahn@gmail.com>
2026-08-24 17:41:28 +0700

    Deprecate shipped components and their context classes

--- lib/components/FuroAccessControlLayout.vue
+++ lib/components/FuroAccessControlLayout.vue
@@ -5,6 +5,9 @@ import {
 
 import FuroAccessControlLayoutContext from '../contexts/concretes/FuroAccessControlLayoutContext.js'
 
+/**
+ * @deprecated since 1.12.0. Will be removed in 2.0.0.
+ */
 export default defineComponent({
   name: 'FuroAccessControlLayout',
   inheritAttrs: false,
--- lib/components/FuroDialog.vue
+++ lib/components/FuroDialog.vue
@@ -12,6 +12,9 @@ const EVENT_NAME = {
   CLICK_BACKDROP: 'clickBackdrop',
 }
 
+/**
+ * @deprecated since 1.12.0. Will be removed in 2.0.0.
+ */
 export default defineComponent({
   name: 'FuroDialog',
   inheritAttrs: false,
